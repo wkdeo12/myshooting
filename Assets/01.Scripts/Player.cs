44dcc7f9b7542c14c834e17b261b3bd6c1d61078
zxsdwqasd <zxsdwqasd@gmail.com>
2020-06-11 00:55:56 +0900

    플레이어 가두기

--- Assets/01.Scripts/Player.cs
+++ Assets/01.Scripts/Player.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Vector2 screenMin;
+    public Vector2 screenMax;
     public VariableJoystick variableJoyStick;
 
     private void Update()
     {
         Vector2 moveVector = new Vector2(variableJoyStick.Horizontal, variableJoyStick.Vertical);
         transform.Translate(moveVector * Time.deltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, screenMin.x, screenMax.x), Mathf.Clamp(transform.position.y, screenMin.y, screenMax.y));
     }
 }
