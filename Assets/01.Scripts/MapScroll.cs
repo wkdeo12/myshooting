a230940d9fc5b1fc2c9732aa0f91c032226fa4cc
zxsdwqasd <zxsdwqasd@gmail.com>
2020-06-17 18:45:03 +0900

    리소스 추가 및 맵 변경

--- Assets/01.Scripts/MapScroll.cs
+++ Assets/01.Scripts/MapScroll.cs
@@ -7,14 +7,18 @@ public class MapScroll : MonoBehaviour
     public float speed = 0.5f;
     public Material material;
 
-    private void Start()
-    {
-    }
+    public GameObject[] backGroundArray;
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
-        float y = material.mainTextureOffset.y + speed * Time.deltaTime;
-        material.mainTextureOffset = new Vector2(0, y);
+        for (int i = 0; i < backGroundArray.Length; i++)
+        {
+            backGroundArray[i].transform.Translate(Vector3.down * speed * Time.fixedDeltaTime);
+            if (backGroundArray[i].transform.position.y <= -6.4f)
+            {
+                backGroundArray[i].transform.position = new Vector3(0, 3.2f, 0);
+            }
+        }
     }
 }
